bb2ee022186ad6dd001fa731ccc216330a8eb35b
aastha-v <59477309+aastha-v@users.noreply.github.com>
2023-07-14 20:14:38 +0530

    Update troubeshooting.sh

--- troubeshooting.sh
+++ troubeshooting.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 #More details at: https://asia.ensembl.org/info/docs/tools/vep/script/vep_download.html
 
+#export current working directory
+export cwd=$(pwd)
+#export lof paths
+export LOF="$cwd/loftee"
+export VEP_PATH=$LOF/vep
+export VEP_DATA=$LOF/vep_data
+export VER=106
+export PERL5LIB=$VEP_PATH:$PERL5LIB
+export PATH=$VEP_PATH/htslib:$PATH
+export PERL5LIB=$VEP_PATH
+export PATH="$VEP_PATH/htslib:$PATH"
+#export PATH=$HOME/vep/samtools/bin:$PATH
+export PERL_BASE="$LOF/perl"
+export PERL5LIB="$PERL_BASE/perl-5.24.1/lib/perl5:$PERL_BASE/perl-5.24.1/lib/perl5/x86_64-linux:$PERL5LIB"
+export PATH="$PERL_BASE/perl-5.24.1/bin:$PATH"
+export PATH="$VEP_PATH:$PATH"
+export KENT_SRC=$LOF/kent-335_base/src
+export MACHTYPE=$(uname -m)
+export CFLAGS="-fPIC"
+export MYSQLINC='mysql_config --include | sed -e 's/^-I//g''
+export MYSQLLIBS='mysql_config --libs'
+export PERL5LIB=$PERL5LIB:$PERL_BASE/cpanm/lib/perl5
+
+#troubleshooting
 export KENT_SRC=$LOF/kent-335_base/src
 export MACHTYPE=$(uname -m)
 export CFLAGS="-fPIC"
